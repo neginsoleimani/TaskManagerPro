4e753456d8280cb0625b706e9a03b58e17cb2f48
Negin Soleiamni <negin.kdk1@gmail.com>
2022-07-09 19:49:05 +0430

    create setting section

---
+++ script/theme.js
@@ -0,0 +1,11 @@
+let settingBtn = document.getElementById("setting-btn");
+let settingBox=document.querySelector(".setting")
+
+
+
+settingBtn.addEventListener("click",setting)
+
+
+function setting (){
+   
+}
